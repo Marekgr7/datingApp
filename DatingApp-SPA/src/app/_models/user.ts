639b55a5a357aa0913b9542b0aaa005d25c449c2
marek <marekgr7@gmail.com>
2020-03-16 12:46:54 +0100

    angular routing and guard added

---
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    id: number;
+    username: string;
+    knowAs: string;
+    age: number;
+    gender: string;
+    created: Date;
+    lastActive: Date;
+    photoUrl: Date;
+    city: string;
+    country: string;
+}
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,6 +4,8 @@ import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -11,13 +13,20 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
@@ -25,6 +34,7 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
       FormsModule,
       BsDropdownModule.forRoot(),
       BrowserAnimationsModule,
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
